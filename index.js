d335af526b965c7d8f415b0f097821d4d54790d6
ASA0190714@ju.edu.jo <ASA0190714@ju.edu.jo>
2023-12-28 20:50:37 +0300

    qaa

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ io.on('connection', (socket) => {
     // Forward the request to the corresponding tanker
     const tankerId = data.tankerId;
     console.log(tankerId);
-    io.emit('tankerRequest', data);
+    io.to(tankerId).emit('tankerRequest', data);
   });
 
   // Handle tanker response
